537dea31dadf89380a3522b9b450e08a9467ed34
Erika7771 <casiraghi.erika@gmail.com>
2020-05-10 00:13:13 +0200

    Arduino reagisce ai comandi

--- Arduino/CAN.ino/CAN.ino.ino
+++ Arduino/CAN.ino/CAN.ino.ino
@@ -6,19 +6,19 @@ struct can_frame canMsg;
 
 MCP2515 mcp2515(10);
 unsigned long prevTime = 0;
-const long interval = 500; //send data each 0.5 seconds
+const long interval = 10; //send data each 0.01 seconds
 
 void setup() {
 
   canMsg1.can_id  = 0x0F6;
   canMsg1.can_dlc = 8;
-  canMsg1.data[0] = 0x8E;
-  canMsg1.data[1] = 0x87;
-  canMsg1.data[2] = 0x32;
+  canMsg1.data[0] = 0x00;
+  canMsg1.data[1] = 0x00;
+  canMsg1.data[2] = 0x00;
   canMsg1.data[3] = 0xFA;
-  canMsg1.data[4] = 0x26;
+  canMsg1.data[4] = 0x00;
   canMsg1.data[5] = 0x8E;
-  canMsg1.data[6] = 0xBE;
+  canMsg1.data[6] = 0x00;
   canMsg1.data[7] = 0x86;
   
   while (!Serial);
@@ -33,26 +33,41 @@ void setup() {
 
 void loop() {
     if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    if(canMsg.can_id !=0x0F6){
-      Serial.println("recieved message: ");
-      Serial.print(canMsg.can_id, HEX); // print ID
-      Serial.print(" "); 
-      Serial.print(canMsg.can_dlc, HEX); // print DLC
-      Serial.print(" ");
-      
-      for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-          
-        Serial.print(canMsg.data[i],HEX);
+      if(canMsg.can_id !=0x0F6){ //own id
+        if(canMsg.can_id ==0x01){
+          Serial.println("recieved message to set speed: ");
+            canMsg1.data[0] = 0x00;
+            canMsg1.data[1] = 0x00;
+            canMsg1.data[2] = 0x00;
+            canMsg1.data[3] = canMsg.data[0];
+        }
+        else if(canMsg.can_id ==0x02){
+          Serial.println("recieved message to set current: ");
+          canMsg1.data[4] = 0x00;
+          canMsg1.data[5] = canMsg.data[0];
+        }
+        else if(canMsg.can_id ==0x03){
+          Serial.println("recieved message to set duty cycle: ");
+          canMsg1.data[6] = 0x00;
+          canMsg1.data[7] = canMsg.data[0];
+        }
+        Serial.print(canMsg.can_id, HEX); // print ID
+        Serial.print(" "); 
+        Serial.print(canMsg.can_dlc, HEX); // print DLC
         Serial.print(" ");
-  
-      }
-      Serial.println(); 
+                
+        for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+            
+          Serial.print(canMsg.data[i],HEX);
+          Serial.print(" ");    
+        }
+        Serial.println(); 
     }     
  }
  if( millis()-prevTime >= interval){
     prevTime = millis();    
     mcp2515.sendMessage(&canMsg1);
-    Serial.println("Messages sent: ");
+    //Serial.println("Messages sent: ");
  }
   
 
